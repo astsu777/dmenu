2e652bcacdd7ec9f6b605b51bb57eb38c5d29951
Gaetan <gaetan@ictpourtous.com>
2021-02-22 04:20:41 +0100

    Set full custom build

---
+++ README.md
@@ -0,0 +1,11 @@
+# My build of DMenu v5.0
+
+This repository hosts the source code of my build of dmenu (Dynamic menu for X) from the [Suckless software](https://tools.suckless.org/dmenu/). It is based on dmenu v5.0 and different patches have been applied in order to provide the features I liked.
+
+**README WILL BE UPDATED LATER TODAY**
+
+# Contact
+You can always reach out to me:
+
+* [Twitter](https://twitter.com/gaetanict)
+* [Email](mailto:gaetan@ictpourtous.com)
---
+++ config.def.h
@@ -0,0 +1,30 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"JetBrains Mono:size=9:antialias=true:autohint:true",
+	"JoyPixels:size=8:antialias=true;autohint:true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cccccc", "#282c34" },
+	[SchemeSel] = { "#ffffff", "#304569" },
+	[SchemeSelHighlight] = { "#b34700", "#000000" },
+	[SchemeNormHighlight] = { "#ff6600", "#000000" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeOutHighlight] = { "#d7d7d7", "#1f2026" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 22;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
